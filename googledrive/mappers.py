0087a52f9751cf034ce5d28b4ad6cb227943069a
Eduardo <garciaruiz.edu@gmail.com>
2020-12-11 17:46:00 +0100

    Credentials as envvar. New 'get' command and extending GoogleFile model. MOre little extensions

--- googledrive/mappers.py
+++ googledrive/mappers.py
@@ -6,8 +6,16 @@ class GoogleFileDictToGoogleFile:
         if google_file_dict is None:
             return None
 
+        name = google_file_dict.get('name') or ''
+        id = google_file_dict.get('id') or ''
+        parents = google_file_dict.get('parents') or []
+        mime_type = google_file_dict.get('mimeType') or ''
+        export_links = google_file_dict.get('exportLinks') or {}
+
         return GoogleFile(
-            name=google_file_dict.get('name'),
-            id=google_file_dict.get('id'),
-            parents=google_file_dict.get('parents'),
+            name=name,
+            id=id,
+            parents=parents,
+            mime_type=mime_type,
+            export_links=export_links
         )
