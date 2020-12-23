bb6ed9a26e0719ea1d4182f03c694d419f8afff6
Eduardo <garciaruiz.edu@gmail.com>
2020-12-23 10:47:37 +0100

    Upgrading google-drive package to 0.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Eduardo'
 author = 'Eduardo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extra_requirements_dev = read_requirements('requirements/dev.txt')
 
 setup(
     name="google-drive",
-    version="0.2.1",
+    version="0.3.0",
     author="Eduardo Garcia",
     author_email="garciaruiz.edu+google-drive-python@gmail.com",
     maintainer="Eduardo García",
